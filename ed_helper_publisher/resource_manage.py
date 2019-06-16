6e24e9519c63cb7260e0cafa3fe1987c888041fc
gary leong <gear@thytruth.com>
2019-06-15 19:40:52 -0700

    updated package

--- ed_helper_publisher/resource_manage.py
+++ ed_helper_publisher/resource_manage.py
@@ -58,7 +58,9 @@ class ResourceCmdHelper(object):
             output = kwargs.get("output")
             if not output: exit(0)
             print '_ed_begin_output'
+            print 'aaa'
             print output
+            print 'bbb'
             print '_ed_end_output'
         else:
             self._print_output(**kwargs)
