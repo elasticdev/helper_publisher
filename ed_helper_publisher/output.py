ed436bebcfdb3eec383e722540e4b0c58547a127
Gary <gary@elasticdev.io>
2019-11-23 14:51:20 -0800

    updated package

--- ed_helper_publisher/output.py
+++ ed_helper_publisher/output.py
@@ -6,6 +6,7 @@ def convert_ed_output_to_values(output):
     values = []
 
     for line in output.split("\n"):
+        if not line: continue
         if "_ed_begin_output" in line:
             record_on = True
             continue
@@ -17,7 +18,14 @@ def convert_ed_output_to_values(output):
 
     print "Try to convert an object"
 
-    obj_return = "\n".join(values)
+    if not values:
+        print 'ERROR: values is None/empty'
+        exit(9)
+
+    if len(values) > 1:
+        obj_return = "\n".join(values)
+    elif len(values) == 1:
+        obj_return = values[0]
 
     try:
         obj_return = convert_str2json(obj_return)
